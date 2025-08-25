e9cc0c98d6bcd26ce0597fcade40b44e431a4323
manugaurdl <manugaurwork@gmail.com>
2025-08-25 09:49:01 +0530

    static dashboard working

---
+++ utils.py
@@ -0,0 +1,178 @@
+from PIL import Image
+import streamlit as st
+import pandas as pd
+import os
+import torch
+import sys
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+@st.cache_data
+def load_ade_data(data_dir):
+    idx2class = {int(_.split("\t")[0]) : _.split("\t")[-1][:-1].strip() for _ in open(os.path.join(data_dir, "objectInfo150.txt"), "r").readlines()[1:]} #0=background, 1=wall
+    ann_dir = os.path.join(data_dir, "annotations/validation")
+    files = os.listdir(ann_dir)
+    class2file = defaultdict(list)
+    for f in files:
+        file = os.path.join(ann_dir, f)
+        seg = np.array(Image.open(file))
+        seg = seg[seg!=0]
+        for obj_class in np.unique(seg):
+            class2file[idx2class[obj_class]].append(f)
+
+    return class2file
+
+@st.cache_data
+def load_df(data_dir):
+    csv_path = os.path.join(data_dir, "referit/refCOCOg", "test.csv")
+    return pd.read_csv(csv_path)
+
+@st.cache_resource
+def init_model(img_size, patch_size, ckpt_dir, ckpt):
+    sys.path.append("/home/manugaur/mllm_inversion")
+    from src.models import FlamingoCrossAttn
+    from src.training import lora_siglip
+    from src.dataloaders.utils import eval_transform
+
+    msg = st.empty()
+    msg.text("| LOADING MODEL...")
+    encoder = FlamingoCrossAttn(
+                visual_encoder="siglip_vitl",
+                text_encoder ="roberta",
+                img_res = img_size,
+                patch_size = patch_size,
+                cross_attn_layers =[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25],
+                cross_attn_ffn_mult =2,
+                )
+
+    lora_siglip(
+        encoder,
+        rank = 8,
+        last_n_blocks=6
+        )
+    trained_wts = torch.load(os.path.join(ckpt_dir, ckpt))['model_state_dict']
+    trained_wts = {k.replace('visual_encoder.siglip.visual.trunk', 'visual_encoder.trunk'): v for k,v in trained_wts.items() if 'visual_encoder.siglip.visual.trunk'}
+
+    state_dict = {}
+    for k,v in encoder.state_dict().items():
+        if k in trained_wts: 
+            state_dict[k] = trained_wts[k]
+        else:
+            state_dict[k] = v
+
+    encoder.load_state_dict(state_dict)
+    msg.empty()
+    # print(f"no of trained weight : {len(trained_wts)}")
+    st.success("Model loaded!")
+    
+    return encoder, eval_transform(img_size)
+
+
+@st.cache_resource
+def roberta_encode(texts, device):
+    roberta = torch.hub.load('pytorch/fairseq', 'roberta.large')
+    roberta.eval();roberta.to(device)
+    class2feat= dict(zip(texts, [roberta.extract_features(roberta.encode(text.lower().strip())) for text in texts]))
+    roberta = None
+    torch.cuda.empty_cache()
+    return class2feat
+
+
+def plot(image):
+    img = Image.open(image_path)
+    st.image(img)
+
+
+def viz_heatmap(img: Image.Image, gt_map: torch.Tensor, pred_map: torch.Tensor):
+    """
+    img: PIL Image
+    gt_map, pred_map: torch.Tensor of shape (16,16)
+    """
+    H = W = 224
+    # 1) Resize image
+    img_resized = img.resize((H, W))
+    img_arr = np.array(img_resized)
+    
+    # 2) Prepare heatmaps
+    if gt_map is not None:
+        gt_tensor = gt_map.float().unsqueeze(0).unsqueeze(0)      # 1×1×16×16
+        
+        gt_resized = (
+            F.interpolate(gt_tensor, size=(H, W), mode='bilinear', align_corners=False)
+            .squeeze()
+            .numpy()
+        )
+    pred_tensor = pred_map.float().unsqueeze(0).unsqueeze(0)  # 1×1×16×16
+    pred_resized = (
+        F.interpolate(pred_tensor, size=(H, W), mode='bilinear', align_corners=False)
+        .squeeze()
+        .numpy()
+    )
+    
+    # 3) Plot
+    num_plots = 2 if gt_map is None else 3
+    fig, axes = plt.subplots(1, num_plots)
+    
+    # Original
+    axes[0].imshow(img_arr)
+    # axes[0].set_title("Original")
+    axes[0].axis("off")
+    
+    # GT overlay
+    if gt_map is not None:
+        axes[1].imshow(img_arr)
+        hm1 = axes[1].imshow(gt_resized, cmap="jet", alpha=0.5)
+        # fig.colorbar(hm1, ax=axes[1], fraction=0.046, pad=0.04)
+        # axes[1].set_title("Ground Truth")
+        axes[1].axis("off")
+    
+    plot_idx = 1 if gt_map is None else 2
+    # Pred overlay
+    axes[plot_idx].imshow(img_arr)
+    hm2 = axes[plot_idx].imshow(pred_resized, cmap="jet", alpha=0.5)
+    # fig.colorbar(hm2, ax=axes[2], fraction=0.046, pad=0.04)
+    # axes[2].set_title("Predicted")
+    axes[plot_idx].axis("off")
+    
+    fig.tight_layout()
+    return fig
+
+
+# def plot_attn_maps(attn_map):
+
+#     attn_map = attn_map.mean(dim=1) # sum over all queries
+#     nh = attn_map.shape[0]
+#     threshold = 0.6
+#     w_featmap = 13
+#     h_featmap = 13
+    
+#     # we keep only a certain percentage of the mass
+#     val, idx = torch.sort(attn_map) # q.K in ascending order
+#     val /= torch.sum(val, dim=1, keepdim=True) # conver to pdf; since we omit cls key, its not a pdf anymore
+#     cumval = torch.cumsum(val, dim=1) # for each head, cumval[i,j] tells how much p_mass is in the bottom half "j" patches 
+#     th_attn = cumval > (1 - threshold) # get patches that have attn_mass within the top end of the distribution
+#     idx2 = torch.argsort(idx)
+#     for head in range(nh):
+#         th_attn[head] = th_attn[head][idx2[head]]
+#     th_attn = th_attn.reshape(nh, w_featmap, h_featmap).float()
+#     # interpolate
+#     th_attn = F.interpolate(th_attn.unsqueeze(0), scale_factor=14, mode="nearest")[0].cpu().numpy()
+#     attn_map = attn_map.reshape(nh, w_featmap, h_featmap)
+#     attn_map = F.interpolate(attn_map.unsqueeze(0), scale_factor=14, mode="nearest")[0].cpu()
+#     attn_map = attn_map.detach().numpy()
+    
+#     # show and save attn_map heatmaps
+#     # torchvision.utils.save_image(torchvision.utils.make_grid(pixel_values, normalize=True, scale_each=True), os.path.join(output_dir, "img.png"))
+    
+#     fig, axs = plt.subplots(4, 4, figsize=(12, 12))  # Adjust figsize as needed
+    
+#     for j in range(nh):
+#         row, col = divmod(j, 4)
+#         axs[row, col].imshow(attn_map[j], cmap='viridis')  # You can change colormap if needed
+#         axs[row, col].set_title(f"Head {j}")
+#         axs[row, col].axis('off')  # Hide axes for a cleaner look
+    
+#     plt.tight_layout()
+#     return fig
---
+++ viz_preds_refCOCO.py
@@ -0,0 +1,82 @@
+import os
+os.environ['TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD'] = '1'
+import random
+import streamlit as st
+st.set_page_config(layout="wide")
+import torch
+from PIL import Image
+from utils import plot, load_df, init_model, viz_heatmap#, plot_attn_maps
+
+st.title("Qualitative Analysis")
+
+#load  refCOCO
+data_dir = "/storage/users/manugaur/mllm_inversion"
+if not os.path.isdir(data_dir): 
+    data_dir = "/workspace/manugaur/mllm_inversion"
+df = load_df(data_dir)
+
+#args
+ckpt_dir = os.path.join(data_dir, "checkpoints")
+ckpt = "vitl14_2.4m_samples_182res_bsz42.pth"
+device = "cuda"
+img_res = 182
+patch_size = 14
+N = img_res//14
+text_feats_dir = os.path.join(data_dir, "referit/refCOCOg/roberta_feats")
+#boolean args
+TEXT_INPUT = True
+
+# refCOCO GT
+softCE_targets = torch.load(os.path.join(os.path.join(data_dir, f"referit/refCOCOg/gauss_maps_256/{N}x{N}_image", f"test.pt"))) #cache them
+
+model, transform = init_model(img_res, patch_size, ckpt_dir, ckpt)
+model.to(device)
+model.eval()
+
+
+### idx to images that weren't seen during training
+proxy_idx = [6, 7, 104, 105, 190, 191, 220, 221, 236, 237, 291, 292, 305, 365, 366, 391, 392, 405, 406, 432, 433, 497, 498, 708, 709, 729, 730, 750, 751, 779, 780, 797, 798, 846, 847, 858, 859, 860, 942, 943, 975, 976, 995, 996, 999, 1000, 1027, 1028, 1142, 1143, 1280, 1281, 1336, 1337, 1375, 1376, 1402, 1403, 1460, 1461, 1537, 1538, 1600, 1601, 1692, 1693, 1710, 1711, 1831, 1832, 1861, 1862, 1904, 1905, 1910, 1911, 1914, 1915, 2157, 2158, 2182, 2183, 2233, 2234, 2293, 2294, 2359, 2360, 2372, 2373, 2388, 2389, 2435, 2436, 2447, 2448, 2584, 2585, 2600, 2601, 2610, 2611, 2614, 2738, 2739, 2872, 2873, 2975, 2976, 3006, 3007, 3166, 3167, 3369, 3370, 3452, 3453, 3473, 3474, 3508, 3509, 3565, 3566, 3608, 3609, 3650, 3651, 3749, 3750, 3775, 3776, 3794, 3795, 3844, 3845, 3846, 3847, 3902, 3903, 3987, 3988, 3991, 3992, 4081, 4248, 4249, 4262, 4263, 4307, 4308, 4311, 4312, 4450, 4451, 4454, 4455, 4471, 4472, 4477, 4478, 4517, 4518, 4554, 4555, 4593, 4594, 4699, 4700, 4719, 4720, 4787, 4788, 4796, 4797, 4831, 4832, 4833, 4834, 4847, 4848, 4886, 4887, 4978, 4979, 5034, 5035, 5065, 5170, 5171, 5252, 5253, 5318, 5319, 5363, 5364, 5493, 5494, 5511, 5512, 5527, 5528, 5576, 5577, 5619, 5620, 5621, 5622, 5623, 5624, 5634, 5635, 5663, 5664, 5723, 5724, 5776, 5777, 5883, 5884, 5897, 5898, 5995, 5996, 6036, 6037, 6054, 6200, 6201, 6204, 6205, 6323, 6324, 6327, 6328, 6389, 6390, 6423, 6424, 6453, 6454, 6517, 6518, 6589, 6590, 6635, 6636, 6825, 6826, 6840, 6841, 6952, 6953, 6958, 6959, 7014, 7015, 7069, 7070, 7179, 7180, 7181, 7182, 7233, 7234, 7313, 7314, 7380, 7381, 7386, 7387, 7392, 7407, 7408, 7423, 7424, 7425, 7426, 7434, 7435, 7471, 7472, 7526, 7527, 7602, 7603, 7607, 7608, 7626, 7669, 7717, 7762, 7814, 7815, 7975, 7976, 8000, 8001, 8002, 8003, 8064, 8065, 8073, 8074, 8118, 8119, 8150, 8151, 8199, 8200, 8261, 8262, 8301, 8302, 8430, 8457, 8458, 8507, 8508, 8575, 8576, 8592, 8611, 8612, 8680, 8681, 8702, 8703, 8768, 8769, 8775, 8776, 8911, 8912, 8917, 8918, 8972, 8973, 8976, 8977, 9023, 9024, 9080, 9120, 9139, 9140, 9167, 9168, 9191, 9192, 9266, 9267, 9272, 9273, 9331, 9332, 9411, 9412, 9415, 9416, 9549, 9550, 9584, 9585]
+
+
+if 'idx' not in st.session_state:
+    st.session_state.idx = 0
+    st.session_state.proxy_idx = proxy_idx[st.session_state.idx]
+    st.session_state.image_path = df.iloc[st.session_state.proxy_idx]["img_path"]
+    st.session_state.caps = df.iloc[st.session_state.proxy_idx]["captions"]
+
+col1, col2 = st.columns([1, 8])
+
+with col1:
+    if st.button("Prev", key="prev_btn"):
+        st.session_state.idx -= 1
+        st.session_state.proxy_idx = proxy_idx[st.session_state.idx]
+        st.session_state.image_path = df.iloc[st.session_state.proxy_idx]["img_path"]
+        st.session_state.caps = df.iloc[st.session_state.proxy_idx]["captions"]
+
+with col2:
+    if st.button("Next", key="next_btn"):
+        st.session_state.idx += 1
+        st.session_state.proxy_idx = proxy_idx[st.session_state.idx]
+        st.session_state.image_path = df.iloc[st.session_state.proxy_idx]["img_path"]
+        st.session_state.caps = df.iloc[st.session_state.proxy_idx]["captions"]
+
+st.write("**GT Caption:**", st.session_state.caps)
+
+#load precomputed text_feats
+text_feat = torch.load(os.path.join(text_feats_dir, 'test', f"{st.session_state.proxy_idx}.pt")).to(device).unsqueeze(0)
+attn_mask = torch.ones((1,text_feat.size(1))).bool().to(text_feat.device)
+
+gt = softCE_targets[st.session_state.proxy_idx]
+gt_heatmap = gt.view((N,N))
+image = Image.open(st.session_state.image_path).convert('RGB')
+
+#get model output
+outputs = model(transform(image).unsqueeze(0).to(device), text_feat, attn_mask).squeeze(0)
+output_heatmap = outputs.softmax(dim=-1).view(N,N)
+# plot outputs
+fig = viz_heatmap(image, gt_heatmap, output_heatmap.detach().cpu())    
+st.pyplot(fig, use_container_width=True)
+
+# attn_map = model.visual_encoder.trunk.blocks[-1].attn.attn_map[0]
+# attn_fig = plot_attn_maps(attn_map)
+# st.pyplot(attn_fig, use_container_width=True)
